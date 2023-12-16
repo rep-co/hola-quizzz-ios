b950b10920208a82d326e1466bcb0a9ea329bed6
Vadim Aleshin <mov4d@MacBook-Air-Vadim.local>
2023-12-16 11:43:32 +0300

    Initial commit

---
+++ quizzz/quizzz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quizzz
+//
+//  Created by Vadim Aleshin on 16.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
