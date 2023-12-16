1f7daf7b709701eecedf35ebd033aa33e6e5880f
SerhiOS <sergeyaslonyns@mail.ru>
2023-12-16 20:25:57 +0300

    main screen added collection

--- quizzz/quizzz/MainScreenViewController/MainScreenCell.swift
+++ quizzz/quizzz/MainScreenViewController/MainScreenCell.swift
@@ -1,27 +1,71 @@
 
 import UIKit
 final class MainScreenCell: UICollectionViewCell{
-    
     struct  Info {
-        var titleLabel: UILabel
-        var descriptionLabel: UILabel
+        var title: String
+        var description: String
     }
     
-
+    var data: Info? {
+        didSet {
+            guard let data else { return }
+            titleLabel.text = data.title
+            descriptionLabel.text = data.description
+        }
+    }
     
+    private let  titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 17)
+        label.textAlignment = .center
+        label.textColor = .black
+        return label
+    }()
 
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 14)
+        label.textAlignment = .center
+        label.textColor = .black
+        return label
+    }()
+    
+    static let identifier = "MainScreenCell"
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+        contentView.backgroundColor = .white
+        contentView.addSubview(descriptionLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(titleLabel)
+        setupConstraints()
+        clipsToBounds = true
+        layer.masksToBounds  = true
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = 16
+    }
 }
+
 private extension MainScreenCell {
-    
-    
-    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            titleLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -10),
+            titleLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 10),
+            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 7),
+            
+            descriptionLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -10),
+            descriptionLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 10),
+            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
+            descriptionLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10)
+        ])
+    }
 }
--- quizzz/quizzz/MainScreenViewController/MainScreenViewController.swift
+++ quizzz/quizzz/MainScreenViewController/MainScreenViewController.swift
@@ -8,48 +8,69 @@
 import UIKit
 import FlyingEmojis
 
-final class MainScreenViewController: UIViewController {
+final class MainScreenViewController: UIViewController,  UICollectionViewDataSource, UICollectionViewDelegate {
     
-    private var collection: UICollectionView?
+    let data = [
+        MainScreenCell.Info(title: "sdfg", description: "gfd" ),
+        MainScreenCell.Info(title: "String", description: "fd" ),
+        MainScreenCell.Info(title: "String", description: "fdfd" ),
+        MainScreenCell.Info(title: "hgfd", description: "fds" ),
+        MainScreenCell.Info(title: "dfg", description: "gfd" ),
+        
+    ]
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
     
-    
+//    private var collectionView = UICollectionView(frame: .zero,
+//                                                  collectionViewLayout: UICollectionViewLayout())
     private let label = UILabel()
     private let imageView = UIImageView()
     private let emojisBackground = ParticleAnimationView()
+    private let layout = UICollectionViewFlowLayout()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+
         addSubvievs()
         setupConstraints()
+        setupLayout()
         setupLable()
         setupImageView()
         setupEmojisBackground()
+        setupCollectionView()
     }
 
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         emojisBackground.updateBounds()
+
     }
 
     func setupLable(){
         label.textColor = .black
-        label.text = "выбери набор"
+        label.text = "SELECT PACK"
         label.textAlignment = .center
-        label.font = .systemFont(ofSize: 40)
+        label.font = .systemFont(ofSize: 30)
     }
     
     func setupImageView(){
-//        imageView.image = .logo
+        imageView.image = .logo
     }
     
     func addSubvievs(){
         label.translatesAutoresizingMaskIntoConstraints = false
         imageView.translatesAutoresizingMaskIntoConstraints = false
         emojisBackground.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(emojisBackground)
         view.addSubview(imageView)
         view.addSubview(label)
+        view.addSubview(collectionView)
     }
 
     func setupConstraints() {
@@ -67,39 +88,50 @@ final class MainScreenViewController: UIViewController {
             label.heightAnchor.constraint(equalToConstant:45),
             label.leftAnchor.constraint(equalTo: view.leftAnchor,constant: 10),
             label.rightAnchor.constraint(equalTo: view.rightAnchor,constant: -10),
-            label.bottomAnchor.constraint(equalTo: imageView.bottomAnchor,constant: 90)
+            label.bottomAnchor.constraint(equalTo: imageView.bottomAnchor,constant: 90),
+            
+            collectionView.topAnchor.constraint(equalTo: label.bottomAnchor,constant: 10),
+            collectionView.leftAnchor.constraint(equalTo: view.leftAnchor,constant: 30),
+            collectionView.rightAnchor.constraint(equalTo: view.rightAnchor,constant: -30),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
             
         ])
     }
+    
 }
 
 // MARK: - Private Methods -
 private extension MainScreenViewController {
+    func setupLayout() {
+        layout.itemSize = CGSize(width: view.frame.size.width/1.2, height: view.frame.size.width/4)
+        layout.scrollDirection = .vertical
+    }
+    
     func setupEmojisBackground() {
         emojisBackground.update(with: .init(mainImage: "🤡".toImage(),
                                             secondaryImage: "🎪".toImage(),
                                             tertiaryImage: "🎠".toImage()))
     }
+
+    func setupCollectionView() {
+        collectionView.backgroundColor = .clear
+        collectionView.register(MainScreenCell.self, forCellWithReuseIdentifier: MainScreenCell.identifier)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.frame = view.bounds
+        collectionView.collectionViewLayout = layout
+        collectionView.showsVerticalScrollIndicator = false
+    }
 }
 
 extension MainScreenViewController{
-    
-//    func setupCollectionView(){
-//        collectionView.delegate = self
-//        collectionView.dataSource = self
-//    }
-    
-}
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
 
-//
-//extension MainScreenViewController: UICollectionViewDataSource, UICollectionViewDelegate {
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        <#code#>
-//    }
-//    
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        <#code#>
-//    }
-//    
-//    
-//}
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MainScreenCell.identifier, for: indexPath)as! MainScreenCell
+        cell.data = self.data[indexPath.row]
+        return cell
+    }
+}
